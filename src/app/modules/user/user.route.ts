503709bec7ad77f4de8beef91c592e225fbe6547
Nuruddin-Adnan <contact.codepixel@gmail.com>
2023-06-14 11:36:52 +0600

    optimize routes, controller, create common catchcatchAsync and sendResponse function

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,13 +1,14 @@
 import express from 'express';
-import { userController } from './user.controller';
+import { UserController } from './user.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { UserValidation } from './user.validation';
+
 const router = express.Router();
 
 router.post(
   '/create-user',
   validateRequest(UserValidation.createUserZodSchema),
-  userController.createUser
+  UserController.createUser
 );
 
 export const UserRoutes = router;
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -3,7 +3,7 @@ import { IUser } from './user.interface';
 import { User } from './user.model';
 import { generateUserId } from './user.utils';
 
-export const createUserService = async (user: IUser): Promise<IUser | null> => {
+export const createUser = async (user: IUser): Promise<IUser | null> => {
   if (!user.password) {
     user.password = config.default_user_password as string;
   }
@@ -17,4 +17,8 @@ export const createUserService = async (user: IUser): Promise<IUser | null> => {
     throw new Error('Fail to create user');
   }
   return result;
+};
+
+export const UserService = {
+  createUser,
 };
