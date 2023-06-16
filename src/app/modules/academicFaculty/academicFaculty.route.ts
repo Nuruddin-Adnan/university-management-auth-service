23791a3ddd6f9a64fae18b9b4fd58e8e99d8a357
Nuruddin-Adnan <contact.codepixel@gmail.com>
2023-06-16 23:17:30 +0600

    faculty CRUD complete

---
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import { AcademicFacultyController } from './academicFaculty.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicFacultyValidation } from './academicFaculty.validation';
+
+const router = express.Router();
+
+router.post(
+  '/create-faculty',
+  validateRequest(AcademicFacultyValidation.createFacultyZodSchema),
+  AcademicFacultyController.createFaculty
+);
+
+router.patch(
+  '/:id',
+  validateRequest(AcademicFacultyValidation.createFacultyZodSchema),
+  AcademicFacultyController.updateFaculty
+);
+
+router.get('/:id', AcademicFacultyController.getSingleFaculty);
+router.delete('/:id', AcademicFacultyController.deleteFaculty);
+router.get('/', AcademicFacultyController.getAllFaculties);
+
+export const AcademicFacultyRoutes = router;
